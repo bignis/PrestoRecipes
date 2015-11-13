bcd88e0833666b8f806fa9b4df46e945ce861ea7
Mike Nistler <bignis@gmail.com>
2015-11-13 10:45:58 -0600

    re-picked this up again, mostly .gitignore changes and app name change

--- build/generated/source/r/debug/com/bignis/prestocookbook/R.java
+++ build/generated/source/r/debug/com/bignis/prestocookbook/R.java
@@ -11,17 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int blue=0x7f040000;
-        public static final int green=0x7f040001;
-        public static final int recipeListRowDivider=0x7f040002;
-        public static final int recipeListRowEven=0x7f040003;
-        public static final int recipeListRowOdd=0x7f040004;
-        public static final int red=0x7f040005;
+        public static final int blue=0x7f050000;
+        public static final int green=0x7f050001;
+        public static final int recipeListRowDivider=0x7f050002;
+        public static final int recipeListRowEven=0x7f050003;
+        public static final int recipeListRowOdd=0x7f050004;
+        public static final int red=0x7f050005;
     }
     public static final class dimen {
-        public static final int IngredientsSize=0x7f050000;
-        public static final int StepsSize=0x7f050001;
-        public static final int TitleSize=0x7f050002;
+        public static final int IngredientsSize=0x7f060000;
+        public static final int StepsSize=0x7f060001;
+        public static final int TitleSize=0x7f060002;
     }
     public static final class drawable {
         public static final int curry=0x7f020000;
@@ -63,31 +63,31 @@ public final class R {
         public static final int load_recipes=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int hello_world=0x7f060002;
-        public static final int menu_load_recipes=0x7f060003;
-        public static final int menu_settings=0x7f060004;
-        public static final int mgn_lorem_ipsum=0x7f060005;
-        public static final int recipe_cake=0x7f060006;
-        public static final int recipe_penne=0x7f060007;
-        public static final int title_activity_load_recipes=0x7f060008;
-        public static final int title_activity_recipes_list=0x7f060009;
+        public static final int action_settings=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int hello_world=0x7f070002;
+        public static final int menu_load_recipes=0x7f070003;
+        public static final int menu_settings=0x7f070004;
+        public static final int mgn_lorem_ipsum=0x7f070005;
+        public static final int recipe_cake=0x7f070006;
+        public static final int recipe_penne=0x7f070007;
+        public static final int title_activity_load_recipes=0x7f070008;
+        public static final int title_activity_recipes_list=0x7f070009;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
-        public static final int AppTheme=0x7f070001;
-        public static final int YourCustomDropDownItemStyle=0x7f070002;
+        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppTheme=0x7f040001;
+        public static final int YourCustomDropDownItemStyle=0x7f040002;
         /**  Here you can set the color and other text attributes 
          */
-        public static final int YourCustomDropDownItemTextStyle=0x7f070003;
+        public static final int YourCustomDropDownItemTextStyle=0x7f040003;
     }
 }
